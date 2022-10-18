3d599014bfcb4e8569f839d04d100f3911189185
Chris Norman <chris.norman2@googlemail.com>
2022-10-18 09:50:57 +0100

    Added more providers.

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:dart_sdl/dart_sdl.dart';
+import 'package:dart_synthizer/dart_synthizer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ziggurat/sound.dart';
+import 'package:ziggurat/ziggurat.dart';
 
 import '../constants.dart';
 import '../src/json/map_level_feature_schema.dart';
@@ -8,6 +13,46 @@ import '../src/json/map_level_schema.dart';
 import '../src/json/project.dart';
 import 'map_level_schema_argument.dart';
 import 'schema_context.dart';
+
+/// Provide an sdl instance.
+final sdlProvider = Provider((final ref) => Sdl());
+
+/// Provides a synthizer instance.
+final synthizerProvider = Provider((final ref) => Synthizer());
+
+/// Provide a synthizer context.
+final synthizerContextProvider = Provider((final ref) {
+  final synthizer = ref.watch(synthizerProvider);
+  return synthizer.createContext();
+});
+
+/// Provide a random number generator.
+final randomProvider = Provider((final ref) => Random());
+
+/// Provide a buffer cache.
+final bufferCacheProvider = Provider(
+  (final ref) {
+    final synthizer = ref.watch(synthizerProvider);
+    final random = ref.watch(randomProvider);
+    return BufferCache(
+      synthizer: synthizer,
+      maxSize: 1.gb,
+      random: random,
+    );
+  },
+);
+
+/// Provide a game.
+final gameProvider = Provider(
+  (final ref) => Game(
+    title: 'Map Level Maker',
+    sdl: ref.watch(sdlProvider),
+    soundBackend: SynthizerSoundBackend(
+      context: ref.watch(synthizerContextProvider),
+      bufferCache: ref.watch(bufferCacheProvider),
+    ),
+  ),
+);
 
 /// Provide the maps which form this project.
 ///
