baa7baeb4bd2026e18ad86a3053f8d363ca653d9
Chris Norman <chris.norman2@googlemail.com>
2022-10-19 15:56:41 +0100

    Clean up after running.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'providers/providers.dart';
 import 'screens/home_page.dart';
 
 void main() => runApp(const MyApp());
@@ -18,7 +19,33 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: const HomePage(),
+          home: const _HomePage(),
         ),
       );
+}
+
+/// The pretend home page to use.
+class _HomePage extends ConsumerStatefulWidget {
+  /// Create an instance.
+  const _HomePage();
+
+  /// Create state for this widget.
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+/// State for [_HomePage].
+class _HomePageState extends ConsumerState<_HomePage> {
+  /// Build a widget.
+  @override
+  Widget build(final BuildContext context) => const HomePage();
+
+  /// Dispose of the widget.
+  @override
+  void dispose() {
+    super.dispose();
+    ref.watch(synthizerContextProvider).destroy();
+    ref.watch(synthizerProvider).shutdown();
+    ref.watch(sdlProvider).quit();
+  }
 }
